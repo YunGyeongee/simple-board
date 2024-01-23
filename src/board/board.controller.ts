51493fe3f44723e15f31ac0cfc00e00db14698b1
yungyeong <yuuung26@gmail.com>
2024-01-23 23:22:54 +0900

    feat: board CRUD, test code

---
+++ src/board/board.controller.ts
@@ -0,0 +1,30 @@
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import { BoardService } from './board.service';
+
+@Controller('board')
+export class BoardController {
+  constructor(private readonly boardService: BoardService) {}
+  @Get()
+  findAll() {
+    return this.boardService.findAll();
+  }
+  @Get(':id')
+  find(@Param('id') id: number) {
+    return this.boardService.find(Number(id));
+  }
+  @Post()
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  create(@Body() data) {
+    return this.boardService.create(data);
+  }
+  @Put(':id')
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  update(@Param('id') id: number, @Body() data) {
+    return this.boardService.update(Number(id), data);
+  }
+  @Delete(':id')
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  remove(@Param('id') id: number) {
+    return this.boardService.remove(Number(id));
+  }
+}
